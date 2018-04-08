a1cc04ce386e3abc06a676cbee9385aa803b367a
Vincent P. Minde <vincentminde@gmail.com>
2018-04-08 10:20:45 +0300

    Added view

--- src/Hris/RecordsBundle/Controller/RecordController.php
+++ src/Hris/RecordsBundle/Controller/RecordController.php
@@ -361,7 +361,7 @@ class RecordController extends Controller
 
         $formEntity = $em->getRepository('HrisFormBundle:Form')->find($id);
 
-        print_r($formEntity);
+        echo $formEntity;
         exit();
 
         $user = $this->container->get('security.context')->getToken()->getUser();
