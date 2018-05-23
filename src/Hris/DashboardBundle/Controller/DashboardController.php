6326c39aa02b542906bc6f1083a31588fe68f1dd
Vincent P. Minde <vincentminde@gmail.com>
2018-05-23 09:57:12 +0300

    Echo

--- src/Hris/DashboardBundle/Controller/DashboardController.php
+++ src/Hris/DashboardBundle/Controller/DashboardController.php
@@ -101,9 +101,8 @@ class DashboardController extends Controller
         //get the data from the aggregation engine
         $organisationunit = $user->getOrganisationunit();
         echo $organisationunit ."<br/>";
-        print_r($forms)  ."<br/>";
         echo $dashboardEmplyomentField ."<br/>";
-        echo $organisationunitGroups ."<br/>";
+        print_r($organisationunitGroups) ."<br/>";
         exit();
         if(empty($organisationunit)) $organisationunit =  $this->getDoctrine()->getManager()->createQuery('SELECT organisationunit FROM HrisOrganisationunitBundle:Organisationunit organisationunit WHERE organisationunit.parent IS NULL')->getSingleResult();
         $dashboardEmplyomentChart = $reportAggregationController::aggregationEngine($organisationunit, $forms,$dashboardEmplyomentField, $organisationunitGroups, TRUE, $dashboardEmplyomentField);
