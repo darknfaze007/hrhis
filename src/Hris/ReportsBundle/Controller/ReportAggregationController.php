88f2aefbebd489e1de939d5033b7a0e348b1bb95
Vincent P. Minde <vincentminde@gmail.com>
2018-05-23 11:04:30 +0300

    Echo

--- src/Hris/ReportsBundle/Controller/ReportAggregationController.php
+++ src/Hris/ReportsBundle/Controller/ReportAggregationController.php
@@ -274,7 +274,7 @@ class ReportAggregationController extends Controller
         $x = 1;
         echo call_user_func(array($selectedOrgunitStructure, 'getLevel2Organisationunit'))->getId()." <br>";
         echo $organisationUnit->getId()." ".'getLevel'.$x.'Organisationunit'."<br>";
-        while($x <= 5) {
+        while($x <= 6) {
             echo $x.". ".$organisationUnit->getId()." == ". call_user_func(array($selectedOrgunitStructure, 'getLevel'.$x.'Organisationunit'))->getId()."<br />";
             if($organisationUnit->getId() == call_user_func(array($selectedOrgunitStructure, 'getLevel'.$x.'Organisationunit'))->getId()){
                 echo "The number is: $x <br>";
