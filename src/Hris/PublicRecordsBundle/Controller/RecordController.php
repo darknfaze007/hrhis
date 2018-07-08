3ade04e440f1784270129405a7ddf2fe7158de3a
Vincent P. Minde <vincentminde@gmail.com>
2018-07-08 11:31:38 +0300

    Fixed security

--- src/Hris/PublicRecordsBundle/Controller/RecordController.php
+++ src/Hris/PublicRecordsBundle/Controller/RecordController.php
@@ -381,8 +381,8 @@ class RecordController extends Controller
             ->orderBy('organisationunit.longname', 'ASC')
             ->getQuery()->getResult();
         $isEntryLevel = true;//$organisationunit->getOrganisationunitStructure()->getLevel()->getDataentrylevel();
-        //echo "here";
-        //exit();
+        echo "here";
+        exit();
         return array(
             'formEntity' => $formEntity,
             'message' => $message,
